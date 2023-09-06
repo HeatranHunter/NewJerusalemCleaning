a35d974624b31322ce72212a45c7f9ed207bd20a
HeatranHunter <131723438+HeatranHunter@users.noreply.github.com>
2023-09-05 23:19:29 -0700

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message = $_POST["message"];
   
   // Send email
-  $to = "contact@newjerusalemcleaning.com";
+  $to = "newjerusalemcleaning@gmail.com";
   $subject = "New Form Submission";
   $messageBody = "Name: $name\nEmail: $email\nPhone #: $phone\nMessage: $message";
   
